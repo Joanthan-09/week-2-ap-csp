7f88af03c9368d1104f60feaed397d9b2dc3bcaa
Joanthan-09 <jonymn011@gmail.com>
2025-10-28 20:04:00 +0000

    Finsihed variable six

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -46,6 +46,12 @@ print("declaration_of_independence:", len(declaration_of_independence))
 # # 3. Indexing and Slicing
 # # ----------------------------------------
 
+chicago_mayor = "Johnson"
+print(len(chicago_mayor))
+print(chicago_mayor[0])
+print(chicago_mayor[4])
+print(chicago_mayor[-1])
+
 # # Indexing: Access characters by position (0-based index)
 # print("First character:", phrase[0])  # Output: P
 # print("Last character:", phrase[-1])  # Output: !
